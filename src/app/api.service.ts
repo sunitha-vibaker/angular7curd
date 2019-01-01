c1508780255149ccf445c4a875fbea92dc65743e
Vibaker <Sunitha.Vibaker@elektrobit.com>
2019-01-01 15:24:39 +0530

    f

---
+++ src/app/api.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Product } from './product';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = "https://sailssv.herokuapp.com/products/list";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) { }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getProducts (): Observable<Product[]> {
+    return this.http.get<Product[]>(apiUrl)
+      
+  }
+  
+  
+  addProduct (product): Observable<Product> {
+    return this.http.post<Product>("https://sailssv.herokuapp.com/products/create", product, httpOptions).pipe(
+      tap((product: Product) => console.log(`added product w/ id=${product._id}`)),
+      catchError(this.handleError<Product>('addProduct'))
+    );
+  }
+  
+  updateProduct (id, product): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, product, httpOptions).pipe(
+      tap(_ => console.log(`updated product id=${id}`)),
+      catchError(this.handleError<any>('updateProduct'))
+    );
+  }
+  
+  deleteProduct (id): Observable<Product> {
+    const url = `${apiUrl}/${id}`;
+  
+    return this.http.delete<Product>(url, httpOptions).pipe(
+      tap(_ => console.log(`deleted product id=${id}`)),
+      catchError(this.handleError<Product>('deleteProduct'))
+    );
+  }
+}
